26a4a3c0c576bf6d1c6b69f98d68f7acc8a95355
Jazmmine <jazmine.angoma@gmail.com>
2016-08-29 07:18:14 -0500

    subiendo

---
+++ app.js
@@ -0,0 +1,11 @@
+var operacion = parseInt(prompt("Escoge una opción:\n" + "1. Sumar\n" + "2. Restar\n" + "3. Multiplicar\n" + "4. Dividir\n" + "5. Resto\n" + "6. Menor que\n" + "7. Mayor que\n" + "8. Menor igual \n" + "7. Mayor igual \n"));
+
+    switch(opcion) {
+        case 'sumar';
+            var numero 1 = parseInt(prompt("Ingrese el #1"));
+            var numero 2 = parseInt(prompt("Ingrese el #2"));
+            var suma = numero1 + numero2;            
+            window.alert("La suma es: " + suma);
+            break;
+
+}
